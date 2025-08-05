33e8bdfb571da5a86a3805d21b2df320f1e39aa0
Auri Gabriel <ririmello.a@gmail.com>
2025-08-05 11:14:37 -0300

    basic operations

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ client.on('error', err => console.log('Erro no Redis', err));
 
 await client.connect();
 
-await client.set('username', 'auri-gabriel');
-const value = await client.get('username');
-console.log(value);
+// SET
+await client.set('username', 'aurigabriel');
+
+// GET
+const user = await client.get('username');
+console.log('Usuário:', user);
+
+// DEL
+await client.del('username');
+
+// INCR (contador)
+await client.set('visitas', 0);
+await client.incr('visitas');
+await client.incr('visitas');
+console.log('Visitas:', await client.get('visitas'));
 
 await client.quit();
